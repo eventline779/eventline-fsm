58f8ccfe032825ff983b228d996ec5582add3309
Leo Balaszeskul <leobalaszeskul@Mac.lan>
2026-04-11 03:06:43 +0200

    Switch font to Helvetica Neue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
 import { Suspense } from "react";
-import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "EVENTLINE FSM",
@@ -35,11 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="de"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">
+    <html lang="de" className="h-full antialiased">
+      <body className="min-h-full flex flex-col font-helvetica">
         <ThemeProvider>
           <Suspense>{children}</Suspense>
         </ThemeProvider>
